4f8abe65cfd7334a0171afd99b69a46a8caf97b2
GuillermoMTorres <Shadowete2@gmail.com>
2017-07-20 16:53:25 +0200

    Siguiente version

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -1,5 +1,36 @@
 window.onload = function () {
 
+  $.get('http://pokeapi.co/api/v2/type/', function( data ) {
+
+    tyu = data;
+  
+    data.results.forEach(function (element){
+      jQuery('<option/>', {
+      value: element.url,
+      text: element.name
+      }).appendTo('#type-selector'); 
+
+    });
+
+  });
+
+var submitType = $('#searchType');
+
+  submitType.on('click', function() {
+
+    $.get( $('#type-selector').val() , function( data ) {
+
+      iop = data; 
+
+      for (var i = 0; i < 10; i++) {
+        console.log(data.pokemon[i].pokemon.name);
+      }
+
+      });
+
+    });
+
+
 var submit = $('#searchId')
 submit.on('click', function(){
 
@@ -9,14 +40,14 @@ submit.on('click', function(){
   asd = data;
   var i = 0;
 
-  jQuery('<div/>', {
+  var carta = jQuery('<div/>', {
       class: 'tarjeta' + id,
-  }).appendTo('.ajaxContainer');
+  })
 
-  jQuery('<h2/>', {
+  var nombre = jQuery('<h2/>', {
       class: 'nombre' +id,
-      text: data.name
-  }).appendTo('.tarjeta' +id);
+      text: data.name 
+  })
 
   var cadena = ""
   data.types.forEach( function (element){
@@ -31,27 +62,30 @@ submit.on('click', function(){
     i++;
   });
 
-      jQuery('<h3/>', {
+     var tipo = jQuery('<h3/>', {
         class: "tipo",
         text: cadena
-    }).appendTo('.tarjeta' + id);
+    })
+
+    $.get('http://pokeapi.co/api/v1/pokemon/'+ id +'/', function( data ) {
+
+      test = data.descriptions[0].resource_uri
+
+      $.get('http://pokeapi.co/'+ test, function( data ) {
+
+      carta.appendTo('.ajaxContainer');
+      nombre.appendTo('.tarjeta' + id);
+      tipo.appendTo('.tarjeta' + id);       
+          jQuery('<p/>', {
+          class: 'description' + id,
+          text: data.description
+      }).appendTo('.tarjeta' + id);
+
+      });
+
+    });
 
 var test = ""
-
-$.get('http://pokeapi.co/api/v1/pokemon/'+ id +'/', function( data ) {
-
-  test = data.descriptions[0].resource_uri
-
-  $.get('http://pokeapi.co/'+ test, function( data ) {
-
-      jQuery('<p/>', {
-      class: 'description' + id,
-      text: data.description
-  }).appendTo('.tarjeta' + id);
-
-  });
-
-});
 
 
 
@@ -62,3 +96,5 @@ $.get('http://pokeapi.co/api/v1/pokemon/'+ id +'/', function( data ) {
 
 
 };
+
+//function()
