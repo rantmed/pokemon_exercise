eabc8ef7aabff5d945db90146416128a25149ce0
Reyes Antequera <rantmed87@gmail.com>
2017-07-20 17:24:39 +0200

    version 4

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -2,8 +2,6 @@ window.onload = function () {
 
   $.get('http://pokeapi.co/api/v2/type/', function( data ) {
 
-    tyu = data;
-  
     data.results.forEach(function (element){
       jQuery('<option/>', {
       value: element.url,
@@ -24,6 +22,7 @@ var submitType = $('#searchType');
 
       for (var i = 0; i < 10; i++) {
         console.log(data.pokemon[i].pokemon.name);
+        cartaTipo(data.pokemon[i].pokemon.url);
       }
 
       });
@@ -38,6 +37,68 @@ submit.on('click', function(){
   $.get('http://pokeapi.co/api/v2/pokemon/'+ $('#inputId').val() +'/', function( data ) {
 
   asd = data;
+  var i = 0;
+
+  var carta = jQuery('<div/>', {
+      class: 'tarjeta' + id,
+  })
+
+  var nombre = jQuery('<h2/>', {
+      class: 'nombre' +id,
+      text: data.name 
+  })
+
+  var cadena = ""
+  data.types.forEach( function (element){
+
+    cadena = cadena + element.type.name;
+
+    if(i!==1 && data.types.length > 1){
+      cadena = cadena + "/";
+    }
+
+
+    i++;
+  });
+
+     var tipo = jQuery('<h3/>', {
+        class: "tipo",
+        text: cadena
+    })
+
+    $.get('http://pokeapi.co/api/v1/pokemon/'+ id +'/', function( data ) {
+
+      var test = data.descriptions[0].resource_uri
+
+      $.get('http://pokeapi.co/'+ test, function( data ) {
+
+      carta.appendTo('.ajaxContainer');
+      nombre.appendTo('.tarjeta' + id);
+      tipo.appendTo('.tarjeta' + id);       
+          jQuery('<p/>', {
+          class: 'description' + id,
+          text: data.description
+      }).appendTo('.tarjeta' + id);
+
+      });
+
+    });
+
+var test = ""
+
+
+
+});
+
+});
+
+
+function cartaTipo(url){
+
+   var id = url.substring(33, url.indexOf("/" , 33 ));
+  console.log("mi id es" + id);
+  $.get(url, function( data ) {
+
   var i = 0;
 
   var carta = jQuery('<div/>', {
@@ -91,8 +152,7 @@ var test = ""
 
 });
 
-});
-
+}
 
 
 };
